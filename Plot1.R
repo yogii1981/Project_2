2403fca571f15d6df43e6fbfe8914830f753c1f1
yogii1981 <yogeshsharma1981@gmail.com>
2015-03-22 11:31:48 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,15 @@
+# Loading the  RDS into Exploratory data in /Users/yogeshsharma/Documents/Data_Scientist/Exploratory_Data_Analysis/Project_2
+# and open theterminal
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+# Samples data for testing purpose
+NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
+
+# Aggregates of Emissions and Emissions$PM
+Emissions <- aggregate(NEI[, 'Emissions'], by = list(NEI$year), FUN = sum)
+Emissions$PM <- round(Emissions[, 2] / 1000, 2)
+
+png(filename = "plot1.png")
+barplot(Emissions$PM, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')))
+dev.off()
