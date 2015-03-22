fdc7803fb4562c0b7defe2936d8624a4345662a6
yogii1981 <yogeshsharma1981@gmail.com>
2015-03-22 11:34:14 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,7 @@
 # Loading the  RDS into Exploratory data in /Users/yogeshsharma/Documents/Data_Scientist/Exploratory_Data_Analysis/Project_2
-# and open theterminal
-NEI <- readRDS("data/summarySCC_PM25.rds")
-SCC <- readRDS("data/Source_Classification_Code.rds")
+# and open the terminal in Project 2
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
 # Samples data for testing purpose
 NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
